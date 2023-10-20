de3529986daf3920bb147f25669fdfa30d880943
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2023-10-20 11:01:25 -0300

    ,~

--- main.js
+++ main.js
@@ -10,4 +10,4 @@ function tocaSomPom(){
     document.querySelector("#som_tecla_clap").play();
 }
 //chamada ou invocacao da funcao tocaSomPom ao clicar no botao
-document.querySelector("tecla_pom").onclick=tocaSomPom;
+document.querySelector("tecla_pom").onclick = tocaSomPom;
