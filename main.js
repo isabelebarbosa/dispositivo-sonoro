c41ea2d8086ef6ed602eb087cea0937842748024
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2023-10-20 11:25:20 -0300

    kj

--- main.js
+++ main.js
@@ -11,3 +11,7 @@ function tocaSomPom(){
 }
 //chamada ou invocacao da funcao tocaSomPom ao clicar no botao
 document.querySelector("tecla_pom").onclick = tocaSomPom;
+
+//criando areferências constante listaDeTeclas e buscando a tecla
+const listaDeTeclas=document.querySelectorAll("tecla");
+//chamando a constante lista De teclas e o indice "0" do SomPom
