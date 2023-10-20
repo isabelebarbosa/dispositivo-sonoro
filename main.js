a17b617adcdfb856d79d589810cea207425a253c
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2023-10-20 10:56:58 -0300

    lçk

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 //declaracao.criacao da funcao tocaSomPom
 function tocaSomPom(){
-    document.que3rySelector("som_tecla_pom").play();
+    document.querySelector("som_tecla_pom").play();
 }
+//chamada ou invocacao da funcao tocaSomPom ao clicar no botao
+    document.querySelector("tecla_pom").onclick=tocaSomPom;
+
+//declaracao.criacao da funcao tocaSomPom
+function tocaSomPom(){
+    document.querySelector("#som_tecla_clap").play();
+}
+//chamada ou invocacao da funcao tocaSomPom ao clicar no botao
+document.querySelector("tecla_pom").onclick=tocaSomPom;
